1c5b0679bacc62c6da70ef9988f6b3eb2e2e05c1
Aishwarya <aishnagare12@gmail.com>
2022-01-12 11:09:55 +0530

    first commit Uploading All Files

---
+++ StringProject/src/com/aishwarya/StringPalindrome.java
@@ -0,0 +1,27 @@
+package com.aishwarya;
+
+import java.util.Scanner;
+
+public class StringPalindrome {
+
+	public static void main(String[] args) {
+String str ,rev="";
+		
+		Scanner scan = new  Scanner(System.in);
+		System.out.println("Enter a String. ");
+	    str = scan.nextLine();
+	   
+	   int length= str.length();
+	   
+	   for (int i = length-1; i >=0; i--) {
+		   rev= rev + str.charAt(i);
+	   }
+		   if(str.equals(rev))
+			   System.out.println(str + " is a palindrome");
+		   else
+			   System.out.println(str +" is not palindrome");
+
+
+	}
+
+}
